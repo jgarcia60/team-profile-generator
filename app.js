20b0909dd02809c77ac2774568e03499a8899424
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-11-08 23:14:16 -0500

    wrote inquirer functions to get a successful output array of employee objects

--- app.js
+++ app.js
@@ -9,7 +9,141 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { array } = require("yargs");
 
+let arrayOfEmployees = [];
+const questions = [
+    {
+        type: 'list',
+        message: "What is this employee's role?",
+        name: 'role',
+        choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
+        type: 'input',
+        message: "What is this employee's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is this employee's ID?",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is this employee's email?",
+        name: 'email',
+    }
+]
+
+function internPrompt(res, arrayOfEmployees) {
+    //ask for name, id, email, school
+    const name = res.name;
+    const id = res.id;
+    const email = res.email;
+    let school;
+    const internQ = [
+        {
+            type: 'input',
+            message: "What is this employee's school?",
+            name: 'school',
+        },
+    ];
+    inquirer.prompt(internQ).then((response) => {
+        console.log(response);
+        school = response.school;
+        const intern = new Intern(name, id, email, school);
+        arrayOfEmployees.push(intern);
+        console.log("Intern prompt:");
+        console.log(arrayOfEmployees);
+        lastCall(arrayOfEmployees);
+    });
+    
+    // return arrayOfEmployees;
+}
+
+function managerPrompt(res, arrayOfEmployees) {
+    //ask for name, id, email, school
+    const name = res.name;
+    const id = res.id;
+    const email = res.email;
+    let officeNumber;
+    const managerQ = [
+        {
+            type: 'input',
+            message: "What is this manager's office number?",
+            name: 'officeNumber',
+        },
+    ];
+    inquirer.prompt(managerQ).then((response) => {
+        officeNumber = response.officeNumber;
+        const manager = new Manager(name, id, email, officeNumber);
+        arrayOfEmployees.push(manager);
+        console.log("Manager prompt:");
+        console.log(arrayOfEmployees);
+        lastCall(arrayOfEmployees);
+    });
+    
+    // return arrayOfEmployees;
+}
+
+function engineerPrompt(res, arrayOfEmployees) {
+    //ask for name, id, email, school
+    const name = res.name;
+    const id = res.id;
+    const email = res.email;
+    let github;
+    const engineerQ = [
+        {
+            type: 'input',
+            message: "What is this employee's github username?",
+            name: 'github',
+        },
+    ];
+    inquirer.prompt(engineerQ).then((response) => {
+        github = response.github;
+        const engineer = new Engineer(name, id, email, github);
+        arrayOfEmployees.push(engineer);
+        console.log("Engineer prompt:");
+        console.log(arrayOfEmployees);
+        lastCall(arrayOfEmployees);
+    });
+    // return arrayOfEmployees;
+}
+
+function lastCall(arrayOfEmployees) {
+    const lastQ = [
+        {
+            type: 'input',
+            message: 'Would you like to add more employees? (Y/N)',
+            name: 'addMore',
+        }
+    ]
+    inquirer.prompt(lastQ).then((response) => {
+        if (response.addMore === 'Y') {
+            init(questions, arrayOfEmployees);
+        } else {
+            //finish making the HTMLs
+            console.log("That's all folks!");
+        }
+    })
+}
+//include 3 functions, one for intern, manager, and engineer. First question is the employees role
+//use an if block to call a function to use a specific inquirer for the given role
+
+// last question is "Would you like to add any more employees?"" If yes, call inquirer
+//again. If no, then use the array of employee objects and call render()
+function init(questions, arrayOfEmployees) {
+    inquirer.prompt(questions).then((response) => {
+        if (response.role === 'intern' || response.role === 'Intern') {
+            arrayOfEmployees = internPrompt(response, arrayOfEmployees);
+        } else if (response.role === 'Manager' || response.role === 'manager') {
+            arrayOfEmployees = managerPrompt(response, arrayOfEmployees);
+        } else if (response.role === 'Engineer' || response.role === 'engineer') {
+            arrayOfEmployees = engineerPrompt(response, arrayOfEmployees);
+        }
+    });
+};
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
@@ -33,3 +167,4 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+init(questions, arrayOfEmployees);
